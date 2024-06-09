dbef717486837b27b84bd586b8f6a3781d3cc913
Otavio077 <88412710+Otavio077@users.noreply.github.com>
2024-06-09 19:25:43 -0300

    correção da barra de navegação

--- src/utils/appUtils.js
+++ src/utils/appUtils.js
@@ -1,26 +1,22 @@
-import _ from 'lodash'
+import _ from 'lodash';
 
 export const returnPaginationRange = (totalPage, page, limit, siblings) => {
-    let totalPageNoInArray = 7 + siblings
-    if(totalPageNoInArray >= totalPage){
+    const totalPageNoInArray = 7 + siblings;
+
+    if (totalPageNoInArray >= totalPage) {
         return _.range(1, totalPage + 1)
     }
 
-    let leftSiblingsIndex = Math.max(page - siblings, 1)
-    let rightSiblingsIndex = Math.min(page + siblings, totalPage)
-    let showLeftDots = leftSiblingsIndex > 2
-    let showRightDots = rightSiblingsIndex < totalPage - 2
-
-    if(!showLeftDots && showRightDots){
-        let leftItensCount = 3 + 2 * siblings
-        let leftRange = _.range(1, leftItensCount + 1)
-        return[...leftRange]
-    } else if (showLeftDots && !showRightDots) {
-        let rightItensCount = 3 + 2 * siblings
-        let rightRange = _.range(totalPage - rightItensCount + 1, totalPage + 1)
-        return [...rightRange]
-    } else {
-        let middleRange = _.range(leftSiblingsIndex -2, rightSiblingsIndex + 3)
-        return [...middleRange]
+    if (page === 1) {
+        return _.range(1, Math.min(4, totalPage + 1))
     }
+
+    if (page === totalPage) {
+        return _.range(Math.max(totalPage - 3, 1), totalPage + 1)
+    }
+
+    const leftIndex = Math.max(page - 3, 1)
+    const rightIndex = Math.min(page + 3, totalPage)
+
+    return _.range(leftIndex, rightIndex + 1)
 }
